5fff3ee3c18e74e9c4b9d1bd8da2b2a3e6049831
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-10-15 01:50:02 +0300

    refactor: update input field ID for transaction description in form

---
+++ script.js
@@ -0,0 +1,8 @@
+const balanceElement = document.getElementById('balance');
+const incomeElement = document.getElementById('income');
+const expenseElement = document.getElementById('expense');
+const transactionList = document.getElementById('history-list');
+const form = document.getElementById('transaction-form');
+const descriptionInput = document.getElementById('description');
+const amountInput = document.getElementById('amount');
+
