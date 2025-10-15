3fb61872a7bc790a2e766d96aeb0c6a63ece8ae9
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-10-15 08:12:17 +0300

    refactor: implement transaction addition functionality and local storage management

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const form = document.getElementById('transaction-form');
 const descriptionInput = document.getElementById('description');
 const amountInput = document.getElementById('amount');
 
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+form.addEventListener('submit', addTransaction);
+
+function addTransaction(e) {
+    e.preventDefault(); // Sayfanın yenilenmesini önler
+    const description = descriptionInput.value.trim();
+    const amount = parseFloat(amountInput.value.trim());
+
+    transactions.push({
+        id:Date.now(),
+        description,
+        amount
+    })
+
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    updateTransactionList();
+    updateBalance();
+    descriptionInput.value = '';
+    amountInput.value = '';
+    
+}
